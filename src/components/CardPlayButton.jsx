6dd4b43ddf4f42bbb9993de8fce9abab756e3774
nahuelfrias45 <nahuelfrias@outlook.com.ar>
2023-10-23 14:23:17 -0300

    implementacion de  lista de canciones a las biblioteclas

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,7 +1,7 @@
 import { Pause, Play } from "./Player"
 import { usePlayerStore } from "@/store/playerStore"
 
-export function CardPlayButton ({ id }) {
+export function CardPlayButton ({ id, size = 'small' }) {
   const { 
     currentMusic, 
     isPlaying, 
@@ -21,16 +21,16 @@ export function CardPlayButton ({ id }) {
         const { songs, playlist } = data
         setIsPlaying(true)
         setCurrentMusic({ songs, playlist, song: songs[0] })
-
-        console.log({ songs, playlist })
       })
   }
 
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id
 
+  const iconClassName = size === 'small' ? 'w-4  h-4' : 'w-5 h-5'
+
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4">
-      {isPlayingPlaylist ? <Pause /> :<Play />}
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+      {isPlayingPlaylist ? <Pause className={iconClassName}/> :<Play className={iconClassName}/>}
     </button>
   )
 }
